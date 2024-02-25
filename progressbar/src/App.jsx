a5fa2b20f6b93e7ab10f7f5346826592ab2f4774
samade747 <samad.e747@gmail.com>
2024-02-25 17:31:32 +0500

    531am 25 2 2024

--- progressbar/src/App.jsx
+++ progressbar/src/App.jsx
@@ -4,11 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <div className='flex justify-center itesm-center flex-col h-80vh'>      
       <div className='flex flex-col items-center justify-center bg-gray-100 w-400 h-300'>
+
+
+      <div className='p-2 m-2'>
+        <input onChange={(e) => setCount(e.target.value)} type="range" value={count} max={100} className='inputRange'  />
+        <label>{count}</label>
+      </div>
+
+    <div className='flex gap-2 m-2 p-5'>
+      <div>
+        <button className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+         onClick={() => dec() }><b>-</b></button>
+      </div>
+
+
+      <div>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+         onClick={() => Inc() }><b>+</b></button>
+      </div>
+      </div>
+
 
 
       </div>  
